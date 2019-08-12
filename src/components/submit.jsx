4d5c8904c62d35bda3f90f62e0758f0351f791e6
David Brooks <davidbrooks@Davids-MacBook-Pro-2.local>
2019-08-11 18:41:52 -0700

    add submit

--- src/components/submit.jsx
+++ src/components/submit.jsx
@@ -12,25 +12,16 @@ class Submit extends React.Component {
         message: '',
 
         }
+        this.sendMessage = this.sendMessage.bind(this);
     }
     sendMessage () {
-      //axios post 
-      config.token 
-      config.AccountSid
-      client.calls.create({
-          url:
-          to: this.state.recipent,
-          from: +14153017919,
-      }, (err,result) => {
-          if (err) {
-              console.log(error)
-          }
-          else {
-              console.log(result)
-          }
+      axios.post('/sms',{
+          recipent: this.state.recipient,
+          message: this.props.message,
       })
     }
 
+    
     render(){
      return(
          <form className='form'>
@@ -39,7 +30,7 @@ class Submit extends React.Component {
              <label>friends name </label>
              <input type='text'></input><br></br>
              <label>send robo call</label>
-             <button className='button'></button>
+             <button onClick ={console.log(this.props.message)} className='button'></button>
          </form>
      )
     }
